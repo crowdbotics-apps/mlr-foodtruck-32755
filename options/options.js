051e1a26a4467952907ddc862fe2467a2c40e461
Crowdbotics <team@crowdbotics.com>
2022-01-09 13:17:17 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,16 +1,54 @@
 import { StyleSheet } from "react-native"
+const styles = StyleSheet.create({
+  hr: {
+    marginTop: 20,
+    marginBottom: 20,
+    borderBottomColor: "black",
+    borderBottomWidth: 1
+  },
 
+  container: {
+    flex: 1,
+    height: 100,
+    padding: 13
+  },
+
+  text: {
+    color: "black",
+    fontSize: 20
+  },
+
+  buttonPressed: {
+    backgroundColor: "aquamarine"
+  },
+
+  buttonNotPressed: {
+    backgroundColor: "blue"
+  },
+
+  button: {
+    borderRadius: 4,
+    padding: 15,
+    marginTop: 10
+  },
+
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16
+  }
+})
 export const globalOptions = {
   name: "mlr_foodtruck_32755",
   url: "https://mlr_foodtruck_32755.botics.co",
   api: "https://mlr_foodtruck_32755.botics.co/api/v1"
 }
-
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    title: "App Menu",
+    copy: "Routes available!",
+    styles: styles
   },
-
   undefined: {
     LOGO_URL:
       "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
