7a7d38eb13d32308dc7d23493bd4a0034b15c0c3
Crowdbotics <team@crowdbotics.com>
2022-01-09 14:17:44 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -78,5 +78,17 @@ export const modulesOptions = {
     SignUpButtonText: "Sign Up"
   },
 
-  "@modules/camera": { buttonText: "+ Take Photo", styles: styles }
+  "@modules/camera": { buttonText: "+ Take Photo", styles: styles },
+  undefined: {
+    LOGO_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
+    BACKGROUND_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
+    HOME_SCREEN_NAME: "Home Screen",
+    validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
+    SignInNavText: "Sign In",
+    SignUpNavText: "Sign Up",
+    SignInButtonText: "Login",
+    SignUpButtonText: "Sign Up"
+  }
 }
